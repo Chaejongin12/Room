f38e3073fa4b6b5a3b703d72ccdf8672890b98ef
채종인 <105656208+Chaejongin12@users.noreply.github.com>
2023-03-16 13:20:12 +0900

    DAO작성

--- app/src/main/java/com/example/room_practice/Word.kt
+++ app/src/main/java/com/example/room_practice/Word.kt
@@ -1,6 +1,8 @@
 package com.example.room_practice
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity
-data class Word(val word: String)
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
